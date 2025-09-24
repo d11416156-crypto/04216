92a6eee5b8f055dd47a2ab9e6003203f8111fba9
d11416156-crypto <d11416156@gs.takming.edu.tw>
2025-09-24 11:07:24 +0800

    BMI V1

---
+++ BMI/BMI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("請輸入體重（公斤）：");
+        string weightInput = Console.ReadLine();
+        double weight = Convert.ToDouble(weightInput);
+
+        Console.Write("請輸入身高（公尺）：");
+        string heightInput = Console.ReadLine();
+        double height = Convert.ToDouble(heightInput);
+
+        double bmi = weight / (height * height);
+
+        Console.WriteLine($"您的 BMI 是：{bmi:F2}");
+    }
+}
